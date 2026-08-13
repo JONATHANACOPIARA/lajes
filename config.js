3914dec87cf1f078a87546149c3cf4bb132d67c2
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2026-08-13 20:52:19 -0300

    Modify comments in config.js for clarity
    Updated comments in config.js to reflect user ID usage.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
+// CONFIGURAÇÃO DEFINITIVA - LAJES PRÉ-MOLDADOS
 const SUPABASE_URL = 'https://dhpoxcaytryoutufgvoo.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRocG94Y2F5dHJ5b3V0dWZndm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2MDI2NjQsImV4cCI6MjEwMjE3ODY2NH0.KL7XEvQtMENa49iwXKp88YhudRhVQFsYUfS5fRn-v9M';
 
-// IMPORTANTE: Cole aqui o UID do seu usuário do Supabase (Authentication > Users)
-// Ele deve ser algo como 'c4789d70-3aa3-47ed-bf7e-a9748bf65973'
+// Este ID vincula as vendas à sua conta no banco de dados
 const OWNER_USER_ID = 'c4789d70-3aa3-47ed-bf7e-a9748bf65973';
