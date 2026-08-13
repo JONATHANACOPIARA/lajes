259f4e0c88bdbcf2864cb0fe2be4a35936354611
JONATHANACOPIARA <criativaaudiovisual@gmail.com>
2026-08-13 11:55:05 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// CONFIGURAÇÃO DO BANCO ONLINE
+// Cole aqui os dados do seu projeto Supabase.
+// ATENÇÃO: A URL do Supabase NÃO deve conter /rest/v1/ no final. Use apenas o domínio base (ex: https://xxx.supabase.co).
+const SUPABASE_URL = 'https://dhpoxcaytryoutufgvoo.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_0SDM5h-3UQf3bZnivCGXkw_Eqno2Ul4';
